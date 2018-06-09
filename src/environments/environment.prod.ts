f6fab566310dc57098055fbf028aa1f207dceef4
Brad Cavanagh <brad.cavanagh@gmail.com>
2018-06-09 08:07:55 -0700

    Update production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-  production: true,
+  production: false,
   googleSpreadsheetKey: '1qHENcVnwTpmZpxgCXMdyQo_z7b56PTSMIU_ZYBMloGs',
-  submitUrl: 'https://r9nj8y2gsl.execute-api.us-west-2.amazonaws.com/prod/submit'
+  submitUrl: 'https://3vu3ve5hv9.execute-api.us-west-2.amazonaws.com/dev/submit',
+  currentProject: 'qboro-tmh'
 };
